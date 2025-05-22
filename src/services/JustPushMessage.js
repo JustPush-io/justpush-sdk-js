84ace144fbd5bcb558aff354c670a1c8f47a3e89
Cristian Urzica <cristianstoianurzica@gmail.com>
2025-05-22 14:37:01 +0300

    feat: [JP-000] add buttonGroup

--- src/services/JustPushMessage.js
+++ src/services/JustPushMessage.js
@@ -1,155 +1,162 @@
-import JustPushBase from '../utils/JustPushBase.js'
+import JustPushBase from "../utils/JustPushBase.js";
 
 export class JustPushMessage extends JustPushBase {
-    static ENDPOINT = '/messages'
+  static ENDPOINT = "/messages";
 
-    constructor(token) {
-        super()
-        this.setToken(token)
-        this.messageParams = {}
-    }
+  constructor(token) {
+    super();
+    this.setToken(token);
+    this.messageParams = {};
+  }
 
-    static token(token = '') {
-        return new JustPushMessage(token)
-    }
+  static token(token = "") {
+    return new JustPushMessage(token);
+  }
 
-    key(messageKey = '') {
-        this.messageParams['key'] = messageKey
-        return this
-    }
+  key(messageKey = "") {
+    this.messageParams["key"] = messageKey;
+    return this;
+  }
 
-    message(message = '') {
-        this.messageParams['message'] = message
-        return this
-    }
+  message(message = "") {
+    this.messageParams["message"] = message;
+    return this;
+  }
 
-    title(title) {
-        this.messageParams['title'] = title
-        return this
-    }
+  title(title) {
+    this.messageParams["title"] = title;
+    return this;
+  }
 
-    topic(topic) {
-        this.messageParams['topic'] = topic
-        return this
-    }
+  topic(topic) {
+    this.messageParams["topic"] = topic;
+    return this;
+  }
 
-    user(user) {
-        this.messageParams['user'] = user
-        return this
-    }
+  user(user) {
+    this.messageParams["user"] = user;
+    return this;
+  }
 
-    image(url, caption = null) {
-        if (!this.messageParams['images']) {
-            this.messageParams['images'] = []
-        }
-        this.messageParams['images'].push({ url, caption })
-        return this
+  image(url, caption = null) {
+    if (!this.messageParams["images"]) {
+      this.messageParams["images"] = [];
     }
+    this.messageParams["images"].push({ url, caption });
+    return this;
+  }
 
-    images(images) {
-        images.forEach((image) => this.image(image.url, image.caption))
-        return this
-    }
+  images(images) {
+    images.forEach((image) => this.image(image.url, image.caption));
+    return this;
+  }
 
-    button(cta, url, actionRequired = false) {
-        if (!this.messageParams['buttons']) {
-            this.messageParams['buttons'] = []
-        }
-        this.messageParams['buttons'].push({ cta, url, actionRequired })
-        return this
+  button(cta, url, actionRequired = false) {
+    if (!this.messageParams["buttons"]) {
+      this.messageParams["buttons"] = [];
     }
+    this.messageParams["buttons"].push({ cta, url, actionRequired });
+    return this;
+  }
 
-    buttons(buttons) {
-        buttons.forEach((button) =>
-            this.button(button.cta, button.url, button.actionRequired)
-        )
-        return this
-    }
+  buttons(buttons) {
+    buttons.forEach((button) =>
+      this.button(button.cta, button.url, button.actionRequired)
+    );
+    return this;
+  }
 
-    sound(sound) {
-        this.messageParams['sounds'] = sound
-        return this
+  buttonGroups(buttonGroups) {
+    if (!this.messageParams["button_groups"]) {
+      this.messageParams["button_groups"] = [];
     }
+    this.messageParams["button_groups"].push(...buttonGroups);
+    return this;
+  }
 
-    priority(priority) {
-        this.messageParams['priority'] = priority
-        return this
-    }
+  sound(sound) {
+    this.messageParams["sounds"] = sound;
+    return this;
+  }
 
-    highestPriority() {
-        return this.priority(2)
-    }
+  priority(priority) {
+    this.messageParams["priority"] = priority;
+    return this;
+  }
 
-    highPriority() {
-        return this.priority(1)
-    }
+  highestPriority() {
+    return this.priority(2);
+  }
 
-    normalPriority() {
-        return this.priority(0)
-    }
+  highPriority() {
+    return this.priority(1);
+  }
 
-    lowPriority() {
-        return this.priority(-1)
-    }
+  normalPriority() {
+    return this.priority(0);
+  }
 
-    lowestPriority() {
-        return this.priority(-2)
-    }
+  lowPriority() {
+    return this.priority(-1);
+  }
 
-    expiry(expiry) {
-        this.messageParams['expiry_ttl'] = expiry
-        return this
-    }
+  lowestPriority() {
+    return this.priority(-2);
+  }
 
-    acknowledge(
-        requiresAcknowledgement,
-        callbackUrl = null,
-        callbackParams = null,
-        requiresRetry = true,
-        retryInterval = 60,
-        maxRetries = 10,
-        callbackRequired = false
-    ) {
-        let retryIntervalPayload = {}
-        if (requiresRetry) {
-            retryIntervalPayload = {
-                requires_retry: requiresRetry,
-                retry_interval: retryInterval,
-                max_retries: maxRetries,
-            }
-        }
-        this.messageParams['acknowledgement'] = {
-            requires_acknowledgement: requiresAcknowledgement,
-            callback_required: callbackRequired,
-            callback_url: callbackUrl,
-            callback_params: JSON.stringify(callbackParams),
-            ...retryIntervalPayload,
-        }
-        return this
-    }
+  expiry(expiry) {
+    this.messageParams["expiry_ttl"] = expiry;
+    return this;
+  }
 
-    async create() {
-        return await this.request(JustPushMessage.ENDPOINT, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(this.messageParams),
-        })
+  acknowledge(
+    requiresAcknowledgement,
+    callbackUrl = null,
+    callbackParams = null,
+    requiresRetry = true,
+    retryInterval = 60,
+    maxRetries = 10,
+    callbackRequired = false
+  ) {
+    let retryIntervalPayload = {};
+    if (requiresRetry) {
+      retryIntervalPayload = {
+        requires_retry: requiresRetry,
+        retry_interval: retryInterval,
+        max_retries: maxRetries,
+      };
     }
+    this.messageParams["acknowledgement"] = {
+      requires_acknowledgement: requiresAcknowledgement,
+      callback_required: callbackRequired,
+      callback_url: callbackUrl,
+      callback_params: JSON.stringify(callbackParams),
+      ...retryIntervalPayload,
+    };
+    return this;
+  }
 
-    async get() {
-        if (!this.messageParams['key']) {
-            throw new Error('Message key must be set before calling get.')
-        }
-        return await this.request(
-            `${JustPushMessage.ENDPOINT}/${this.messageParams['key']}`,
-            {
-                method: 'GET',
-            }
-        )
-    }
+  async create() {
+    return await this.request(JustPushMessage.ENDPOINT, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(this.messageParams),
+    });
+  }
 
-    getMessageParams() {
-        return this.messageParams
+  async get() {
+    if (!this.messageParams["key"]) {
+      throw new Error("Message key must be set before calling get.");
     }
+    return await this.request(
+      `${JustPushMessage.ENDPOINT}/${this.messageParams["key"]}`,
+      {
+        method: "GET",
+      }
+    );
+  }
+
+  getMessageParams() {
+    return this.messageParams;
+  }
 }
-
--- src/tests/Message.js
+++ src/tests/Message.js
@@ -1,75 +1,128 @@
-import { JustPushMessage } from '../services/JustPushMessage.js'
-import { ACCESS_TOKEN } from './TestConstants.js'
+import { JustPushMessage } from "../services/JustPushMessage.js";
+import { ACCESS_TOKEN } from "./TestConstants.js";
 
-const IMAGE_PAYLOAD = [
-    'https://i2.wp.com/www.learnsteps.com/wp-content/uploads/2017/04/what-is-a-hyperlink-with-pictures-UDZ66a-clipart.jpg?fit=1000%2C740&ssl=1',
-    'Test caption',
-]
+const IMAGE_PAYLOAD = ["https://picsum.photos/800/600", "Test caption"];
 
 const sendSimpleTextMesage = async () => {
-    console.log('Send simple text message...')
-    return await JustPushMessage.token(ACCESS_TOKEN)
-        .topic('TestTopic')
-        .title('Test Title')
-        .message('Here is a sample Message')
-        .create()
-}
+  console.log("Send simple text message...");
+  return await JustPushMessage.token(ACCESS_TOKEN)
+    .topic("TestTopic")
+    .title("Test Title")
+    .message("Here is a sample Message")
+    .create();
+};
 
 const sendImageMesage = async () => {
-    console.log('Send image message...')
-    return await JustPushMessage.token(ACCESS_TOKEN)
-        .topic('TestTopic')
-        .title('Test Title')
-        .message('Here is a massage with images')
-        .image(IMAGE_PAYLOAD[0], IMAGE_PAYLOAD[1])
-        .image(IMAGE_PAYLOAD[0], IMAGE_PAYLOAD[1])
-        .image(IMAGE_PAYLOAD[0], IMAGE_PAYLOAD[1])
-        .image(IMAGE_PAYLOAD[0], IMAGE_PAYLOAD[1])
-        .image(IMAGE_PAYLOAD[0], IMAGE_PAYLOAD[1])
-        .create()
-}
+  console.log("Send image message...");
+  return await JustPushMessage.token(ACCESS_TOKEN)
+    .topic("TestTopic")
+    .title("Test Title")
+    .message("Here is a massage with images")
+    .image(IMAGE_PAYLOAD[0], IMAGE_PAYLOAD[1])
+    .image(IMAGE_PAYLOAD[0], IMAGE_PAYLOAD[1])
+    .image(IMAGE_PAYLOAD[0], IMAGE_PAYLOAD[1])
+    .image(IMAGE_PAYLOAD[0], IMAGE_PAYLOAD[1])
+    .image(IMAGE_PAYLOAD[0], IMAGE_PAYLOAD[1])
+    .create();
+};
 
 const sendAckMesage = async () => {
-    console.log('Send ack message...')
-    return await JustPushMessage.token(ACCESS_TOKEN)
-        .topic('TestTopic')
-        .title('Test Title')
-        .message('Here is a message with ack')
-        .acknowledge(true, 'https://www.google.ro')
-        .create()
-}
+  console.log("Send ack message...");
+  return await JustPushMessage.token(ACCESS_TOKEN)
+    .topic("TestTopic")
+    .title("Test Title")
+    .message("Here is a message with ack")
+    .acknowledge(true, "https://www.google.ro")
+    .create();
+};
 
 const sendButtonsMesage = async () => {
-    console.log('Send button message...')
-    return await JustPushMessage.token(ACCESS_TOKEN)
-        .topic('TestTopic')
-        .title('Test Title')
-        .message('Here is a message with ack')
-        .acknowledge(true, 'https://www.google.ro')
-        .button('Button 1', 'https://google.com', true)
-        .create()
-}
+  console.log("Send button message...");
+  return await JustPushMessage.token(ACCESS_TOKEN)
+    .topic("TestTopic")
+    .title("Test Title")
+    .message("Here is a message with ack")
+    .acknowledge(true, "https://www.google.ro")
+    .button("Button 1", "https://google.com", true)
+    .create();
+};
+
+const sendButtonGroupsMessage = async () => {
+  console.log("Send button groups message...");
+  return await JustPushMessage.token(ACCESS_TOKEN)
+    .topic("TestTopic")
+    .title("Test Title")
+    .message("Here is a message with button groups")
+    .buttonGroups([
+      {
+        name: "Group 1",
+        cta: "Group 1 Cta",
+        action_required: true,
+        buttons: [
+          {
+            cta: "Button 1",
+            url: "https://google.com",
+            action_required: true,
+          },
+          {
+            cta: "Button 2",
+            url: "https://google.com",
+            action_required: true,
+          },
+          {
+            cta: "Button 3",
+            url: "https://google.com",
+            action_required: true,
+          },
+        ],
+      },
+      {
+        name: "Group 2",
+        cta: "Group 2 Cta",
+        action_required: true,
+        buttons: [
+          {
+            cta: "Button 1",
+            url: "https://google.com",
+            action_required: true,
+          },
+          {
+            cta: "Button 2",
+            url: "https://google.com",
+            action_required: true,
+          },
+          {
+            cta: "Button 3",
+            url: "https://google.com",
+            action_required: true,
+          },
+        ],
+      },
+    ])
+    .create();
+};
 
 const getMessage = async (messageKey) => {
-    console.log('Get message...', messageKey)
-    return await JustPushMessage.token(ACCESS_TOKEN).key(messageKey).get()
-}
+  console.log("Get message...", messageKey);
+  return await JustPushMessage.token(ACCESS_TOKEN).key(messageKey).get();
+};
 
 const test = async () => {
-    try {
-        await sendSimpleTextMesage()
-        await sendImageMesage()
-        await sendAckMesage()
-        const lastMessage = await sendButtonsMesage()
+  try {
+    await sendSimpleTextMesage();
+    await sendImageMesage();
+    await sendAckMesage();
+    await sendButtonGroupsMessage();
+    const lastMessage = await sendButtonsMesage();
 
-        // Wait for 5 seconds
-        await new Promise((resolve) => setTimeout(resolve, 8000))
+    // Wait for 5 seconds
+    await new Promise((resolve) => setTimeout(resolve, 8000));
 
-        // Retrieve the message
-        await getMessage(lastMessage.key)
-    } catch (error) {
-        console.error('Error:', error)
-    }
-}
+    // Retrieve the message
+    await getMessage(lastMessage.key);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
 
-test()
+test();
